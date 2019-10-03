f64f931f42dc8678ea3206043b2ec5c5b5350e2f
Ubuntu <ubuntu@ip-172-31-25-71.ec2.internal>
2019-10-03 18:17:33 +0000

    positive-negative program

---
+++ positive-negative.py
@@ -0,0 +1,24 @@
+#!user/bin/env python3
+
+# Created by: RJ Fromm
+# Created on: September 2019
+# This program can determine if a number is 0, positive or negative
+
+
+def main():
+
+    number = int(input("Enter any number : "))
+
+    if number > 0:
+        print("Your number is positive")
+
+    elif number == 0:
+        print("Your number is zero")
+
+    else:
+        number < 0
+        print("Your number is negative")
+
+
+if __name__ == "__main__":
+    main()
